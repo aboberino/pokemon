2d60edfaad0a1a29de6712789950cdade8d6f5b0
ba608204 <aimeric.boban@etu.unice.fr>
2020-04-26 20:06:07 +0200

    Ajout des stats dans la page de détail pokemon

--- projectbase/Models/PokemonModel.cs
+++ projectbase/Models/PokemonModel.cs
@@ -27,5 +27,54 @@ namespace projectbase.Models
 
 
         public PokemonModel() { }
+
+        public string getType()
+        {
+            switch (Types[0].Type.Name)
+            {
+                case "normal":
+                    return "https://www.pokebip.com/pages/icones/types/3G/normal.png";
+                case "fighting":
+                    return "https://www.pokebip.com/pages/icones/types/3G/combat.png";
+                case "flying":
+                    return "https://www.pokebip.com/pages/icones/types/3G/vol.png";
+                case "poison":
+                    return "https://www.pokebip.com/pages/icones/types/3G/poison.png";
+                case "ground":
+                    return "https://www.pokebip.com/pages/icones/types/3G/sol.png";
+                case "rock":
+                    return "https://www.pokebip.com/pages/icones/types/3G/roche.png";
+                case "bug":
+                    return "https://www.pokebip.com/pages/icones/types/3G/insecte.png";
+                case "steel":
+                    return "https://www.pokebip.com/pages/icones/types/3G/acier.png";
+                case "fire":
+                    return "https://www.pokebip.com/pages/icones/types/3G/feu.png";
+                case "water":
+                    return "https://www.pokebip.com/pages/icones/types/3G/eau.png";
+                case "grass":
+                    return "https://www.pokebip.com/pages/icones/types/3G/plante.png";
+                case "electric":
+                    return "https://www.pokebip.com/pages/icones/types/3G/electrik.png";
+                case "psychic":
+                    return "https://www.pokebip.com/pages/icones/types/3G/psy.png";
+                case "ice":
+                    return "https://www.pokebip.com/pages/icones/types/3G/glace.png";
+                case "dragon":
+                    return "https://www.pokebip.com/pages/icones/types/3G/dragon.png";
+                case "dark":
+                    return "https://www.pokebip.com/pages/icones/types/3G/tenebres.png";
+                case "fairy":
+                    return "";
+                case "shadow":
+                    return "";
+                case "ghost":
+                    return "";
+
+                default:
+                    return "https://www.pokebip.com/pages/icones/types/3G/normal.png";
+            }
+
+        }
     }
 }
