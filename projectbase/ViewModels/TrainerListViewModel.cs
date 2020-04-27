5e7d4c8bdad41d5a9c2ee8701a76179ba534d90b
ba608204 <aimeric.boban@etu.unice.fr>
2020-04-27 10:16:58 +0200

    Ajout de la possibilité de créer un nouveau trainer

--- projectbase/ViewModels/TrainerListViewModel.cs
+++ projectbase/ViewModels/TrainerListViewModel.cs
@@ -2,7 +2,9 @@
 using projectbase.Views;
 using System;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 using Xamarin.Forms;
+using System.Diagnostics;
 
 namespace projectbase
 {
@@ -10,6 +12,7 @@ namespace projectbase
     {
         ObservableCollection<Trainer> trainers;
         public ObservableCollection<Trainer> Trainers { get => trainers; set => SetProperty(ref trainers, value); }
+
         Trainer trainerSelected;
         public Trainer TrainerSelected
         {
@@ -34,46 +37,28 @@ namespace projectbase
             setData();
         }
 
+        public ICommand OnClickedAddTrainerButton { get => new Command(AddTrainerButtonClicked); }
+
+        async void AddTrainerButtonClicked()
+        {
+            string name = await Application.Current.MainPage.DisplayPromptAsync("Add Trainer", "Enter a Name");
+            Trainer newTrainer = new Trainer
+            {
+                Name = name,
+                Sprite = "https://myuu.xyz/images/trainers/Psychic-F-A.png",
+                Pokemon1 = "charizard",
+                Pokemon2 = "articuno",
+                Pokemon3 = "mewtwo",
+                Pokemon4 = "dragonair",
+                Pokemon5 = "dragonite",
+                Pokemon6 = "pidgeot"
+            };
+            await App.Database.SaveTrainerAsync(newTrainer);
+            Trainers = new ObservableCollection<Trainer>(await App.Database.GetTrainerAsync());
+        }
+
         async void setData()
         {
-            Trainer t1 = new Trainer()
-            {
-                Name = "Rancher",
-                Sprite = "https://myuu.xyz/images/trainers/Rancher.png",
-                Pokemon1 = "charizard",
-                Pokemon2 = "pikachu",
-                Pokemon3 = "ivysaur",
-                Pokemon4 = "ditto",
-                Pokemon5 = "bulbasaur",
-                Pokemon6 = "pidgeot"
-
-            };
-            Trainer t2 = new Trainer()
-            {
-                Name = "Dancer",
-                Sprite = "https://myuu.xyz/images/trainers/Dancer.png",
-                Pokemon1 = "pidgeotto",
-                Pokemon2 = "pikachu",
-                Pokemon3 = "ivysaur",
-                Pokemon4 = "ditto",
-                Pokemon5 = "bulbasaur",
-                Pokemon6 = "pidgeot"
-            };
-            Trainer t3 = new Trainer()
-            {
-                Name = "Swimmer-M-B",
-                Sprite = "https://myuu.xyz/images/trainers/Swimmer-M-B.png",
-                Pokemon1 = "pidgeotto",
-                Pokemon2 = "pikachu",
-                Pokemon3 = "ivysaur",
-                Pokemon4 = "ditto",
-                Pokemon5 = "bulbasaur",
-                Pokemon6 = "pidgeot"
-            };
-            await App.Database.DeleteAllTrainerAsync();
-            await App.Database.SaveTrainerAsync(t1);
-            await App.Database.SaveTrainerAsync(t2);
-            await App.Database.SaveTrainerAsync(t3);
             Trainers = new ObservableCollection<Trainer>(await App.Database.GetTrainerAsync());
         }
     }
